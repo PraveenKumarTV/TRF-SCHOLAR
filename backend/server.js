5f9f377b04ce2a7c995c30d2687c07cf9e0d71a2
Praveen Kumar T V <160822105+PraveenKumarTV@users.noreply.github.com>
2026-02-03 06:38:45 +0530

    Serve static files from the build directory

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const PORT=5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "../build")));
 app.post('/login',(req,res)=>{
     const {email,password,role}=req.body;
     const snapshot=db.collection("scholarDetails")
