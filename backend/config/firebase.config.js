4e677bdd269db33b508224134db455a51c3e96be
Praveen Kumar T V <160822105+PraveenKumarTV@users.noreply.github.com>
2026-02-03 06:13:36 +0530

    Update Firebase service account configuration

--- backend/config/firebase.config.js
+++ backend/config/firebase.config.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../service_account_key.json');
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
