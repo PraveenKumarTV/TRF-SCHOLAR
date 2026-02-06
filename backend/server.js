2060ff3de5d070e519c95fc428505b5ce7d3153a
Praveen Kumar T V <160822105+PraveenKumarTV@users.noreply.github.com>
2026-02-06 21:22:08 +0530

    Update path for serving index.html in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, "../dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 app.post('/login',(req,res)=>{
     const {email,password,role}=req.body;
